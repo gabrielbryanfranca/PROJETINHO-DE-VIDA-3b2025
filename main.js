8e4e765f3178b591a33cb389c1f0c34bfd25ccb6
gabrielbryanfranca <gabriel.bryan.franca@escola.pr.gov.br>
2025-05-06 16:22:52 -0300

    1622

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
+for (let i = 0; i < botoes.length; I++)
 
-for (let i = 0; i < botoes.length; i++)
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
 
-    botoes[i].onclick = function () {
-
-        botoes [i].classList.add("ativo");
     }
